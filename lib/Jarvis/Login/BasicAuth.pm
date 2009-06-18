d9cd876fb2d60ef25c013d150e7f76de64d6b4e9
jcouper <none@none>
2009-06-18 04:01:24 +0000

    New module to check BasicAuth, including SSL/HTTPS
    --HG--
    branch : JARVIS_REST

---
+++ lib/Jarvis/Login/BasicAuth.pm
@@ -0,0 +1,155 @@
+###############################################################################
+# Description:
+#       Jarvis supports pluggable Login modules.  This module checks server
+#	environment variables to determine if the user has:
+#	
+#	  - Basic Authentication remote user is as expected, and
+#	  - OPTIONALLY: Has the correct remote IP address, and
+#	  - OPTIONALLY: Accessed via HTTPS 
+#
+#	For the last case, the server may have required them client
+#	present a client certificate, but we don't know about that.
+#
+#	Note that to get certificate names mapping to BasicAuth, you
+# 	can try the following http.conf.  See:
+#
+#		http://httpd.apache.org/docs/2.2/ssl/ssl_howto.html
+#
+#   <Directory /usr/local/apache2/htdocs/secure/area>
+# 	SSLVerifyClient      require
+# 	SSLVerifyDepth       5
+# 	SSLCACertificateFile conf/ssl.crt/ca.crt
+# 	SSLCACertificatePath conf/ssl.crt
+# 	SSLOptions           +FakeBasicAuth
+# 	SSLRequireSSL
+# 	AuthName             "Snake Oil Authentication"
+# 	AuthType             Basic
+# 	AuthBasicProvider    file
+# 	AuthUserFile         /usr/local/apache2/conf/httpd.passwd
+# 	Require              valid-user
+#   </Directory>
+#
+#	Then in httpd.passwd put:
+#
+#	/C=NZ/ST=Hastings/O=Woogloo/CN=Woogloo for N-Squared Access:xxj31ZMTZzkVA
+#
+#       Refer to the documentation for the "check" function for how
+#       to configure your <application>.xml to use this login module.
+#
+# Licence:
+#       This file is part of the Jarvis WebApp/BasicAuth gateway utility.
+# 
+#       Jarvis is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+# 
+#       Jarvis is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+# 
+#       You should have received a copy of the GNU General Public License
+#       along with Jarvis.  If not, see <http://www.gnu.org/licenses/>.
+#
+#       This software is Copyright 2008 by Jonathan Couper-Smartt.
+###############################################################################
+#
+use CGI;
+
+use strict;
+use warnings;
+
+use Jarvis::Error;
+
+package Jarvis::Login::BasicAuth;
+
+###############################################################################
+# Public Functions
+###############################################################################
+
+################################################################################
+# Determines if we are "logged in".  In this case we look at CGI variables.
+# The username is not actually offered by the client.  Instead, we have a single
+# user, and we have a hard-coded username and group for them.
+#
+# Note that this plugin allows only a SINGLE user specification.  In the future
+# we may want to distinguish between different users.  My solution for that is
+# to allow multiple "Login" blocks in the top-level configuration file.  If
+# we have many users, each with their own unique client certificates, then we
+# probably need a custom module.  We'll deal with that if and when it arrives.
+#
+# In each parameter entry, you MUST specify username.  Group list is optional,
+# if missing, the user will be placed in a single group with groupname equal to
+# the username.  The CN is the common name.  It is currently treated as an
+# EXACT match against the CN on the certificate.  The IP address must be an
+# exact match if present.
+#
+# To use this method, specify the following login parameters.
+#  
+#    <app use_placeholders="yes" format="json" debug="no">
+#        ...
+#        <login module="Jarvis::Login::BasicAuth">
+#  	     <parameter name="require_https" value="yes"/>
+#  	     <parameter name="remote_ip" value="192.168.1.1"/>
+#	     <parameter name="remote_user" value="Exact Common Name"/>
+#  	     <parameter name="username" value="default"/>
+#  	     <parameter name="group_list" value="default"/>
+#        </login>
+#        ...
+#    </app>
+#
+# Params:
+#       $jconfig - Jarvis::Config object
+#           READ
+#               cgi
+#               BasicAuth config indirectly via Jarvis::DB
+#   
+#       %login_parameters - Hash of login parameters parsed from
+#               the master application XML file by the master Login class.
+#       
+#
+# Returns:
+#       ($error_string or "", $username or "", "group1,group2,group3...")
+################################################################################
+#
+sub Jarvis::Login::BasicAuth::check {
+    my ($jconfig, $username, $password, %login_parameters) = @_;
+
+    # Our user name login parameters are here...
+    my $require_https = defined ($Jarvis::Config::yes_value {lc ($login_parameters{'require_https'} || "no")});
+    my $remote_ip = $login_parameters{'remote_ip'} || '';
+    my $remote_user = $login_parameters{'remote_user'} || '';
+    $username = $login_parameters{'username'};
+    my $group_list = $login_parameters{'group_list'} || $username;
+
+    # No check on username/password.  We don't require or expect them.  WE will
+    # determine the username.
+    if ($require_https && ! $jconfig->{'cgi'}->https()) {
+        return ("Client must access over HTTPS for this login method.");
+    }
+
+    # Check the IP address first.
+    if ($remote_ip ne '') {
+        my $actual_ip = $ENV{"HTTP_X_FORWARDED_FOR"} || $ENV{"HTTP_CLIENT_IP"} || $ENV{"REMOTE_ADDR"} || '';
+	if ($actual_ip ne $remote_ip) {
+            return ("Access not authorized from actual remote IP address.");
+	}
+    }
+
+    # Now check the user.
+    if ($remote_user ne '') {
+        my $actual_ip = $ENV{"HTTP_X_FORWARDED_FOR"} || $ENV{"HTTP_CLIENT_IP"} || $ENV{"REMOTE_ADDR"} || '';
+	if ($actual_ip ne $remote_ip) {
+            return ("Access not authorized from actual remote IP address.");
+	}
+    }
+
+    if (! $username) {
+        return ("Missing configuration for Login module BasicAuth.");
+    }
+
+    return ("", $username, $group_list);
+}
+
+1;
