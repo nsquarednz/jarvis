9d1a83470f157f80c4447ddd4f19466580e9276b
jcouper <none@none>
2009-06-23 02:50:11 +0000

    Doc complete, time to proof
    --HG--
    branch : JARVIS_REST

--- demo/plugin/Demo.pm
+++ demo/plugin/Demo.pm
@@ -6,8 +6,12 @@ sub plugin::Demo::do {
 
     my $output = "";
 
+    &Jarvis::Error::debug ($jconfig, "Running the Demo plugin.");
+
     # This demonstrates that the plugin can access Jarvis::Config info.
-    $output = "APP: " . $jconfig->{'app_name'} . "\n";
+    $output .= "APP: " . $jconfig->{'app_name'} . "\n";
+    $output .= "Username: " . $jconfig->{'username'} . "\n";
+    $output .= "CGI myval: " . ($jconfig->{'cgi'}->param('myval') || '') . "\n";
 
     # This demonstrates XML-configured parameters to the plugin.
     $output .= "CAT: " . $args{'category'} . "\n";
