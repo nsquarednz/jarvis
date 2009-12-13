73ab268a4aaf0b4364bb965bfc1c7c27b955d63d
jcouper <none@none>
2009-12-13 21:31:23 +0000

    New database

---
+++ stats/db/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE request (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    app TEXT,
+    username TEXT,
+    group_list TEXT,
+    dataset TEXT,
+    params TEXT,
+    made_at JULIAN,
+    took_ms INTEGER
+);
+
+CREATE INDEX request_made_at_idx on request (made_at);
