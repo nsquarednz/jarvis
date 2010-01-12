5a90ed248026231a3e671090b1da2dc4e2ecb125
jcouper <none@none>
2010-01-12 01:00:11 +0000

    Add new dataset_type column to requests.

--- tracker/db/patch001.sql
+++ tracker/db/patch001.sql
@@ -4,9 +4,11 @@ ALTER TABLE request ADD COLUMN dataset_type TEXT (1);
 CREATE TABLE login (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sid TEXT,
+    app_name TEXT,
     username TEXT,
     logged_in INTEGER,
     error_string TEXT,
     group_list TEXT,
+    address TEXT,
     start_time JULIAN
 );
