769d06a76e6a942ab387d6e4a3cb11ea42cbdee8
jcouper <none@none>
2010-01-16 00:23:53 +0000

    We don't always have store.url, but we do always have store.proxy.url

--- demo/htdocs/include/common.js
+++ demo/htdocs/include/common.js
@@ -254,7 +254,7 @@ Ext.override (Ext.data.Store, {
      */
     handleWriteback : function (result, ttype, record, remain) {
         if (result.success != 1) {
-            var dataset_name = this.url.replace (jarvis_home + '/' + application + '/', '');
+            var dataset_name = this.proxy.url.replace (jarvis_home + '/' + application + '/', '');
             alert ("Database error for " + ttype + " on '" + dataset_name + "':\n" + result.message);
             return 0;
 
@@ -285,7 +285,7 @@ Ext.override (Ext.data.Store, {
      */
     handleWritebackArray : function (result, ttype, records, remain) {
         if (result.success != 1) {
-            var dataset_name = this.url.replace (jarvis_home + '/' + application + '/', '');
+            var dataset_name = this.proxy.url.replace (jarvis_home + '/' + application + '/', '');
             alert ("Database error for " + ttype + " on '" + dataset_name + "':\n" + result.message);
             return 0;
 
