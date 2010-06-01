6f051b9b7da19a2c9578e6bf5fe34199b2945c54
jcouper <none@none>
2010-06-01 23:09:47 +0000

    Allow Drupal6 login module to borrow existing Drupal cookies.

--- lib/Jarvis/Login/Drupal6.pm
+++ lib/Jarvis/Login/Drupal6.pm
@@ -15,6 +15,7 @@
 #    <app format="json" debug="no">
 #        ...
 #        <login module="Jarvis::Login::Drupal">
+#            <parameter name="allow_login" value="yes"/>
 #            <parameter name="admin_only" value="yes"/>
 #        </login>
 #        ...
@@ -94,51 +95,97 @@ package Jarvis::Login::Drupal6;
 sub Jarvis::Login::Drupal6::check {
     my ($jconfig, $username, $password, %login_parameters) = @_;
 
-    # Config.
+    # Allow Login.
+    #  yes = We accept a supplied username/password and perform user/password checking.
+    #
+    #  no  = We require that the Drupal login procedure be used.  We only create a Jarvis
+    #        session when we see a Drupal session has been created.
+    #
+    my $allow_login = defined ($Jarvis::Config::yes_value {lc ($login_parameters{'allow_login'} || "no")});
     my $admin_only = defined ($Jarvis::Config::yes_value {lc ($login_parameters{'admin_only'} || "no")});
 
-    # No info?
-    $username || return ("No username supplied.");
-    $password || return ("No password supplied.");
+    # See if we can find a Drupal session.
+    my $logged_in = 0;
+    my $uid = undef;
 
-    my $dbh = &Jarvis::DB::handle ($jconfig);
+    my %cookies = fetch CGI::Cookie;
+    &Jarvis::Error::debug ($jconfig, "Checking for any existing Drupal session cookie.");
 
-    # Check the username from the user name table.
-    my $result_aref = $dbh->selectall_arrayref("SELECT uid, pass FROM users WHERE name = ? AND status = 1", { Slice => {} }, $username);
-    if ((scalar @$result_aref) < 1) {
-        return ("User '$username' not known/active.");
-    }
-    if ((scalar @$result_aref) > 1) {
-        return ("User '$username' not unique (" . (scalar @$result_aref). ").");         # Should never happen.
-    }
-    my $result_href = $$result_aref[0];
-    my $uid = $$result_href{'uid'} || '';
-    my $stored_password = $$result_href{'pass'} || '';
+    foreach my $cookie_name (keys %cookies) {
+        next if $cookie_name !~ m/^SESS/;
+        my $cookie_value = $cookies{$cookie_name}->value;
 
-    if ($stored_password eq '') {
-        return ("Account has no password.");
-    }
-    $uid || die "No uid for user '$username'!";
+        &Jarvis::Error::debug ($jconfig, "Checking existing Drupal sid '$cookie_value'.");
+        my $dbh = &Jarvis::DB::handle ($jconfig);
+        my $result_aref = $dbh->selectall_arrayref("SELECT u.uid, u.name FROM sessions s INNER JOIN users u ON u.uid = s.uid WHERE s.sid = ?", { Slice => {} }, $cookie_value);
 
+        if ((scalar @$result_aref) >= 1) {
+            my $result_href = $$result_aref[0];
+            $uid = $$result_href{'uid'} || die "No uid for sid '$cookie_value'!";
 
-    # Check the password.  MD5.  NO SALT!
-    # Yes, I know this is vulnerable to dictionary, but that's Drupal's decision not mine.
-    #
-    if (length ($stored_password) != 32) {
-        return ("Stored password is invalid length for MD5");
-    }
-    my $md5_salt_prefix_len = 0;
-    my $salt = substr ($stored_password, 0, $md5_salt_prefix_len);
-    my $stored_md5 = substr ($stored_password, $md5_salt_prefix_len);
+            &Jarvis::Error::debug ($jconfig, "Found existing Drupal session for uid $uid, name '$username'.");
+            if ($admin_only && ($uid != 1)) {
+                &Jarvis::Error::debug ($jconfig, "Ignoring this one, we require an admin session.");
+                $uid = undef;
 
-    if (&Digest::MD5::md5_hex ($salt . $password) ne $stored_md5) {
-        return ("Incorrect password hash.");
+            } else {
+                $username = $$result_href{'name'} || die "No uid for sid '$cookie_value'!";
+                $logged_in = 1;
+                last;
+            }
+
+        } else {
+            &Jarvis::Error::debug ($jconfig, "No such Drupal session (or unknown uid).");
+        }
     }
 
-    # Check admin_only flag.
-    if ($admin_only && ($uid != 1)) {
-        return ("Must be Drupal initial admin user.");
+    # See if a username/password has been supplied and use that.
+    if (! $logged_in) {
+        $allow_login || return ("Drupal login is required.");
+        &Jarvis::Error::debug ($jconfig, "Trying the Jarvis login process for Drupal.");
+
+        $username || return ("No username supplied.");
+        $password || return ("No password supplied.");
+
+        # Check the username from the user name table.
+        my $dbh = &Jarvis::DB::handle ($jconfig);
+        my $result_aref = $dbh->selectall_arrayref("SELECT uid, pass FROM users WHERE name = ? AND status = 1", { Slice => {} }, $username);
+        if ((scalar @$result_aref) < 1) {
+            return ("User '$username' not known/active.");
+        }
+        if ((scalar @$result_aref) > 1) {
+            return ("User '$username' not unique (" . (scalar @$result_aref). ").");         # Should never happen.
+        }
+        my $result_href = $$result_aref[0];
+        $uid = $$result_href{'uid'} || die "No uid for user '$username'!";
+        my $stored_password = $$result_href{'pass'} || '';
+
+        if ($stored_password eq '') {
+            return ("Account has no password.");
+        }
+
+        # Check the password.  MD5.  NO SALT!
+        # Yes, I know this is vulnerable to dictionary, but that's Drupal's decision not mine.
+        #
+        &Jarvis::Error::debug ($jconfig, "Checking password hash.");
+        if (length ($stored_password) != 32) {
+            return ("Stored password is invalid length for MD5");
+        }
+        my $md5_salt_prefix_len = 0;
+        my $salt = substr ($stored_password, 0, $md5_salt_prefix_len);
+        my $stored_md5 = substr ($stored_password, $md5_salt_prefix_len);
+
+        if (&Digest::MD5::md5_hex ($salt . $password) ne $stored_md5) {
+            return ("Incorrect password hash.");
+        }
+        &Jarvis::Error::debug ($jconfig, "Password hash valid.  Checking admin requirements.");
+        if ($admin_only && ($uid != 1)) {
+            return ("Must be Drupal initial admin user.");
+        }
     }
+
+    # By now we MUST have logged in.
+    $uid || die;
 
     # Set groups list.
     my $group_list = ($uid == 1) ? "admin,user" : "user";
