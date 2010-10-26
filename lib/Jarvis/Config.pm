2166b52542ffa850a70131dc2f8b830bd49041c8
jcouper <none@none>
2010-10-26 05:27:29 +0000

    Find a way that doesn't give those damn 'clean' error messages from XML

--- lib/Jarvis/Config.pm
+++ lib/Jarvis/Config.pm
@@ -138,11 +138,15 @@ sub new {
     # Pull out the list of default (Perl) library paths to use for perl plugins scripts
     # from the configuration, and store in an array in the config item.
     $self->{'default_libs'} = [];
-    if ($axml->{'default_libs'}) {
-        my @libs = $axml->{default_libs}{lib}('@');
-        my @paths = map { $_->{'path'} } @libs;
-        $self->{'default_libs'} = \@paths;
+    if ($axml->{'default_libs'}{'lib'}) {
+        foreach my $lib ($axml->{'default_libs'}{'lib'}('@')) {
+            &Jarvis::Error::debug ($self, "Default Lib Path: " . ($lib->{'path'}->content || 'UNDEFINED'));
+            if ($lib->{'path'}->content) {
+                push (@{ $self->{'default_libs'} }, $lib->{'path'}->content);
+            }
+        }
     }
+
 
     # Basic security check here.
     $self->{'require_https'} = defined ($Jarvis::Config::yes_value {lc ($axml->{'require_https'}->content || "no")});
