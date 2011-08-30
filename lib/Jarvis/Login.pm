9a374f249b43a04502c0b44bfaab01c2163faeb8
Henning Koehler <hkoehler@nsquared.co.nz>
2011-08-30 15:51:16 +1200

    Issue 3192 - bugfix: we want to prevent username/password as URL parameters only, not as CGI parameters in general
    --HG--
    branch : ISSUE_3192

--- lib/Jarvis/Login.pm
+++ lib/Jarvis/Login.pm
@@ -163,17 +163,19 @@ sub check {
         $jconfig->{'sid_param'} = '';
     }
 
-    # require-post flag. If true, we prohibit username/password as cgi parameters
+    # require-post flag. If true, we prohibit username/password as url parameters
     # this is to prevent them from getting logged (e.g. by apache)
     my $login_requires_post = defined ($Jarvis::Config::yes_value {lc ($axml->{'login'}{'require-post'} || 'no')});
-    
+    # by this stage URL parameters have already been copied into POST parameters
     my $cgi_username = $jconfig->{'cgi'}->param('username');
     my $cgi_password = $jconfig->{'cgi'}->param('password');
+    my $url_username = $jconfig->{'cgi'}->url_param('username');
+    my $url_password = $jconfig->{'cgi'}->url_param('password');
     my $require_post_error = undef;
 
-    if ($login_requires_post and ($cgi_username or $cgi_password)) {
-        &Jarvis::Error::log ($jconfig, "Username/password provided as CGI parameters when require-post was specified (removed).");
-        $require_post_error = "username/password provided as CGI parameters when require-post was specified";
+    if ($login_requires_post and ($url_username or $url_password)) {
+        &Jarvis::Error::log ($jconfig, "Username/password provided as URL parameters when require-post was specified (removed).");
+        $require_post_error = "username/password provided as URL parameters when require-post was specified";
         $cgi_username = undef;
         $cgi_password = undef;
     }
@@ -268,7 +270,7 @@ sub check {
         {
             no strict 'refs';
             ($error_string, $username, $group_list, $additional_safe) = &$login_method ($jconfig, $offered_username, $offered_password, %login_parameters);
-            # login is likely to fail if CGI username/password are deleted due to require-post
+            # login is likely to fail if URL username/password are deleted due to require-post
             if ($require_post_error) {
                 $error_string = ($error_string ? "$error_string ($require_post_error)" : "Login ok, but $require_post_error");
             }
