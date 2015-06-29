8fd631083db8acb53da7f22d4987eb02782a83a2
Daniel Walmsley <daniel.walmsley@nsquaredsoftware.com>
2015-06-29 14:07:34 +1200

    ISSUE 8314 allow production version of jarvis work with the latest XML smart lib.
    XML smart now uses wantarray so you have to be careful of the context when calling the
    content method on XML. It would seem that inside the map function the context gets set
    to array somehow. By explicitly setting this to go in a scalar variable it seems to set
    the context correctly.
    
    --HG--
    branch : SUSTAINING_5_4_10

--- lib/Jarvis/DB/SDP.pm
+++ lib/Jarvis/DB/SDP.pm
@@ -1,13 +1,13 @@
 ##############################################################################
 # Implements Jarvis::DB::SDP
 #
-# This class implements a pseudo database interaction with the SSAS 
+# This class implements a pseudo database interaction with the SSAS
 # DataPump HTTP(S) service.
 #
 # This is invoked with
-#  <database type="sdp" ... > 
+#  <database type="sdp" ... >
 #
-# in the Jarvis application config file. 
+# in the Jarvis application config file.
 ##############################################################################
 #
 use strict;
@@ -35,17 +35,17 @@ BEGIN {
 
 sub new {
     my ($class, $jconfig, $connect, $username, $password, $parameters) = @_;
-   
+
     my $self = {};
     bless $self, $class;
-    
+
     $self->init ($jconfig, $connect, $username, $password, $parameters);
     return $self;
 }
 
 sub init {
     my ($self, $jconfig, $connect, $username, $password, $parameters) = @_;
-    
+
     $self->{'connect'} = $connect;
     $self->{'username'} = $username;
     $self->{'password'} = $password;
@@ -53,13 +53,13 @@ sub init {
 
     # Create the client SOAP handle.
     my $soap = SOAP::Lite->new( proxy => $connect);
-    
+
     $soap->readable(1);
     $soap->autotype(0);
     $soap->outputxml(1);
     $soap->default_ns('urn:schemas-microsoft-com:xml-analysis');
     $soap->transport->http_request->header ('Authorization' => 'Basic ' . MIME::Base64::encode("$username:$password"));
-    $self->{'soap'} = $soap;    
+    $self->{'soap'} = $soap;
 }
 
 ##############################################################################
@@ -85,11 +85,11 @@ sub disconnect {
 ################################################################################
 #
 sub fetchall {
-    
+
     my ($self, $jconfig, $mdx) = @_;
-    
+
     &Jarvis::Error::debug ($jconfig, "Performing Execute request to SSAS DataPump.");
-    
+
     my @property_list = ();
     if ($self->{'parameters'}->{'catalog'}) {
         push (@property_list,
@@ -98,9 +98,9 @@ sub fetchall {
             )
         );
     };
-                    
+
     my $soap = $self->{'soap'};
-    my $rxml = $soap->call('Execute',         
+    my $rxml = $soap->call('Execute',
         SOAP::Data->name('Command')->value(
             \SOAP::Data->value(
                 SOAP::Data->name('Statement')->type('string')->value($mdx)
@@ -114,27 +114,27 @@ sub fetchall {
             )
         )
     );
-    
+
     # Check for errors.
     my $response = XML::Smart->new ($rxml) || die "Malformed XML on SDP response.";
     $response->{'soap:Envelope'}->{'soap:Body'} || die "No soap:Envelope/soap:Body found in SDP response.";
-    
+
     if ($response->{'soap:Envelope'}->{'soap:Body'}->{'soap:Fault'}) {
         my $description = $response->{'soap:Envelope'}->{'soap:Body'}->{'soap:Fault'}->{'detail'}->{'Error'}->{'Description'}->content || 'SOAP Fault returned from SDP request.';
         my $error_code = $response->{'soap:Envelope'}->{'soap:Body'}->{'soap:Fault'}->{'detail'}->{'Error'}->{'ErrorCode'}->content || 'unknown';
         my $source = $response->{'soap:Envelope'}->{'soap:Body'}->{'soap:Fault'}->{'detail'}->{'Error'}->{'Source'}->content || 'SSAS DataPump';
         die "$source returned error $error_code: $description";
     }
-    
-    return $response->{'soap:Envelope'}->{'soap:Body'}->{'ExecuteResponse'}->{'return'}->{'root'} 
+
+    return $response->{'soap:Envelope'}->{'soap:Body'}->{'ExecuteResponse'}->{'return'}->{'root'}
         || die "Missing ExecuteResponse/return/root in SDP response.";
 }
-    
+
 ################################################################################
 # Makes the specified MDX request, and converts to similar structure to DBI.
 #
 # This function only processes a single dataset.  The parent method may invoke
-# us multiple times for a single request, and combine into a single return 
+# us multiple times for a single request, and combine into a single return
 # object.
 #
 # Params:
@@ -149,56 +149,56 @@ sub fetchall {
 #
 sub fetchall_arrayref {
     my ($self, $jconfig, $mdx, $row_label) = @_;
-    
+
     # Get the raw data.
     my $root = $self->fetchall ($jconfig, $mdx);
-    
+
     # Check for errors.
-    &Jarvis::Error::dump ($jconfig, $root->data); 
+    &Jarvis::Error::dump ($jconfig, $root->data);
     if ($root->{'Exception'}) {
         my $code = $root->{'Messages'}->{'Error'}->{'ErrorCode'} || '???';
         my $description  = $root->{'Messages'}->{'Error'}->{'Description'} || '???';
         die "An MDX error $code occured: $description";
     }
-    
+
     # Otherwise, assume we have data.
     &Jarvis::Error::debug ($jconfig, "Converting 2D MDX result to Array of tuples.");
 
     # Now the fun bit.  Convert the deep complicated structure into 2D tuple array like DBI would.
     my @axis_names = grep { $_ ne 'SlicerAxis' } map { $_->{'name'}->content } $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}('@');
     foreach my $axis_name (@axis_names) {
-        &Jarvis::Error::debug ($jconfig, "Axis: $axis_name");        
+        &Jarvis::Error::debug ($jconfig, "Axis: $axis_name");
     }
     (scalar @axis_names == 2) || die "Require exactly two (non-Slice) Axes for 2D tuple encoding.  Got " . (scalar @axis_names) . ".";
 
-    # What are the names for the column/row axes?    
+    # What are the names for the column/row axes?
     my $column_axis_label = $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}[0]->{'HierarchyInfo'}->{'name'}->content || "Unknown Axis0 Name";
     my $row_axis_label = $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}[1]->{'HierarchyInfo'}->{'name'}->content || "Unknown Axis1 Name";
     &Jarvis::Error::debug ($jconfig, "Column Axis Label = $column_axis_label");
     &Jarvis::Error::debug ($jconfig, "Row Axis Label = $row_axis_label");
-    
+
     # What are the tuple names?
     ($root->{'Axes'}->{'Axis'}[0]->{'name'}->content eq 'Axis0') || die "Inconsistent Axis0 Name";
     ($root->{'Axes'}->{'Axis'}[1]->{'name'}->content eq 'Axis1') || die "Inconsistent Axis1 Name";
-    
-    my @column_names = map { $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[0]->{'Tuples'}{'Tuple'}('@');
+
+    my @column_names = map { my $tmp = $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[0]->{'Tuples'}{'Tuple'}('@');
     my $num_columns = scalar @column_names;
-    
-    my @row_names = map { $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[1]->{'Tuples'}{'Tuple'}('@');
-    my $num_rows = scalar @row_names;    
-    
+
+    my @row_names = map { my $tmp = $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[1]->{'Tuples'}{'Tuple'}('@');
+    my $num_rows = scalar @row_names;
+
     # Pre-fill the rows.
     &Jarvis::Error::debug ($jconfig, "Have $num_columns columns, $num_rows rows.");
     my @rows = ();
     foreach my $i (0 .. ($num_rows - 1)) {
         if ($row_label) {
             push (@rows, {$row_label => $row_names[$i]});
-            
+
         } else {
             push (@rows, '');
         }
     }
-    
+
     # Now the cell data
     foreach my $cell ($root->{'CellData'}->{'Cell'}('@')) {
         my $ordinal = $cell->{'CellOrdinal'}->content;
@@ -206,7 +206,7 @@ sub fetchall_arrayref {
         my $column = $ordinal % $num_columns;
         my $row = ($ordinal - $column) / $num_columns;
         my $column_name = $column_names[$column];
-        $rows[$row]->{$column_name} = $value; 
+        $rows[$row]->{$column_name} = $value;
     }
 
     # Finally put the row label column at the start of the column list.
@@ -218,10 +218,10 @@ sub fetchall_arrayref {
 }
 
 ################################################################################
-# Makes the specified MDX request, and returns an 3D nested hash.  
+# Makes the specified MDX request, and returns an 3D nested hash.
 #
 # This function only processes a single dataset.  The parent method may invoke
-# us multiple times for a single request, and combine into a single return 
+# us multiple times for a single request, and combine into a single return
 # object.
 #
 # Params:
@@ -236,53 +236,53 @@ sub fetchall_arrayref {
 #
 sub fetchall_hashref_3d {
     my ($self, $jconfig, $mdx, $row_label) = @_;
-    
+
     # Get the raw data.
     my $root = $self->fetchall ($jconfig, $mdx);
-    
+
     &Jarvis::Error::debug ($jconfig, "Converting 3D MDX result to Array of nested tuples.");
 
     # Now the fun bit.  Convert the deep complicated structure into 2D tuple array like DBI would.
     my @axis_names = grep { $_ ne 'SlicerAxis' } map { $_->{'name'}->content } $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}('@');
     foreach my $axis_name (@axis_names) {
-        &Jarvis::Error::debug ($jconfig, "Axis: $axis_name");        
+        &Jarvis::Error::debug ($jconfig, "Axis: $axis_name");
     }
     (scalar @axis_names == 3) || die "Require exactly three (non-Slice) Axes for 3D tuple encoding.  Got " . (scalar @axis_names) . ".";
 
-    # What are the names for the column/row axes?    
+    # What are the names for the column/row axes?
     my $column_axis_label = $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}[0]->{'HierarchyInfo'}->{'name'}->content || "Unknown Axis0 Name";
     my $row_axis_label = $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}[1]->{'HierarchyInfo'}->{'name'}->content || "Unknown Axis1 Name";
     my $page_axis_label = $root->{'OlapInfo'}->{'AxesInfo'}->{'AxisInfo'}[2]->{'HierarchyInfo'}->{'name'}->content || "Unknown Axis2 Name";
     &Jarvis::Error::debug ($jconfig, "Column Axis Label = $column_axis_label");
     &Jarvis::Error::debug ($jconfig, "Row Axis Label = $row_axis_label");
     &Jarvis::Error::debug ($jconfig, "Page Axis Label = $page_axis_label");
-    
+
     # What are the tuple names?
     ($root->{'Axes'}->{'Axis'}[0]->{'name'}->content eq 'Axis0') || die "Inconsistent Axis0 Name";
     ($root->{'Axes'}->{'Axis'}[1]->{'name'}->content eq 'Axis1') || die "Inconsistent Axis1 Name";
     ($root->{'Axes'}->{'Axis'}[2]->{'name'}->content eq 'Axis2') || die "Inconsistent Axis2 Name";
-    
+
     my @column_names = map { $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[0]->{'Tuples'}{'Tuple'}('@');
     my $num_columns = scalar @column_names;
     foreach my $column (@column_names) {
-        &Jarvis::Error::dump ($jconfig, "Column: " . $column);        
+        &Jarvis::Error::dump ($jconfig, "Column: " . $column);
     }
-    
+
     my @row_names = map { $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[1]->{'Tuples'}{'Tuple'}('@');
-    my $num_rows = scalar @row_names;    
+    my $num_rows = scalar @row_names;
     foreach my $row (@row_names) {
-        &Jarvis::Error::dump ($jconfig, "Row: " . $row);        
+        &Jarvis::Error::dump ($jconfig, "Row: " . $row);
     }
 
     my @page_names = map { $_->{'Member'}->{'Caption'}->content } $root->{'Axes'}->{'Axis'}[2]->{'Tuples'}{'Tuple'}('@');
-    my $num_pages = scalar @page_names;    
+    my $num_pages = scalar @page_names;
     foreach my $page (@page_names) {
-        &Jarvis::Error::dump ($jconfig, "Page: " . $page);        
+        &Jarvis::Error::dump ($jconfig, "Page: " . $page);
     }
-    
+
     # Pre-fill the rows.
     &Jarvis::Error::debug ($jconfig, "Have $num_columns columns, $num_rows rows, $num_pages pages.");
-    
+
     # Now the cell data
     my %data = ();
     foreach my $cell ($root->{'CellData'}->{'Cell'}('@')) {
@@ -293,15 +293,15 @@ sub fetchall_hashref_3d {
 
         my $row = $ordinal % $num_rows;
         $ordinal = ($ordinal - $row) / $num_rows;
-        
+
         my $page = $ordinal;
-        
+
         &Jarvis::Error::debug ($jconfig, "Index $ordinal -> C/R/P = $column, $row, $page.");
-        
+
         my $column_name = $column_names[$column];
         my $row_name = $row_names[$row];
         my $page_name = $page_names[$page];
-        $data{$page_name}{$row_name}{$column_name} = $value; 
+        $data{$page_name}{$row_name}{$column_name} = $value;
     }
 
     return (\%data);
