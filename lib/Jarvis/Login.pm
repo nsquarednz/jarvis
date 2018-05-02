13b0792b743f560ebdd19c96e233a2ebb14826d5
Marcel Kroll <marcel@nsquaredsoftware.com>
2018-05-02 17:24:37 +1200

    ISSUE #13400 Strip ports when dealing with non standard ports from cookie domain.

--- lib/Jarvis/Login.pm
+++ lib/Jarvis/Login.pm
@@ -371,12 +371,17 @@ sub check {
             # url based sessions. We get the best of both worlds.
             #
             if (!$jconfig->{'sid_source'} || $jconfig->{'sid_source'} eq 'cookie') {
+
+                # Get the session cookie domain. In some non standard cases this can contain a port number which we 
+                # cannot include in the cookie domain so we need to strip this accodingly
+                my $session_cookie_domain = ($jconfig->{'scookie_domain'} =~ /^([^:^\/]*)(:\\d*)?(.*)?$/gm)[0];
+
                 my $cookies = [ CGI::Cookie->new (
                     -name => $jconfig->{'sname'},
                     -value => $jconfig->{'sid'},
                     -HttpOnly => 1,
                     -Path => $jconfig->{'scookie_path'},
-                    -domain => $jconfig->{'scookie_domain'},
+                    -domain => $session_cookie_domain,
                     -secure => $jconfig->{'scookie_secure'},
                     -samesite => 'Strict'   # Note - only supported on 4.29 of CGI::Cookie or later
                 )];
@@ -391,7 +396,7 @@ sub check {
                             -value => $additional_cookies->{$key},
                             -HttpOnly => 1,
                             -Path => $jconfig->{'scookie_path'},
-                            -domain => $jconfig->{'scookie_domain'},
+                            -domain => $session_cookie_domain,
                             -secure => $jconfig->{'scookie_secure'},
                             -samesite => 'Strict'   # Note - only supported on 4.29 of CGI::Cookie or later
                         ))
@@ -408,7 +413,7 @@ sub check {
                         -name => $jconfig->{csrf_cookie},
                         -value => $jconfig->{session}->param ('csrf_token'),
                         -Path => '/',           # Note, left as / as many clients use differing URLs to access applications, and apps are from /
-                        -domain => $jconfig->{'scookie_domain'},
+                        -domain => $session_cookie_domain,
                         -secure => $jconfig->{'scookie_secure'},
                         -samesite => 'Strict'   # Note - only supported on 4.29 of CGI::Cookie or later
                     ));
